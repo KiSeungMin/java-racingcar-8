f87e9d01ecae9a6d09ae36457af83582ceb8a29c
KiSeungMin <ksm011212@gmail.com>
2025-10-21 17:36:12 +0900

    feat(InputParser): 입력 라운드에 대한 예외 처리 추가
    - 라운드 입력이 Integer로 표현할 수 있는 최댓값보다 클 때
    - 라운드 입력이 0보다 작은 음수일 때

--- src/main/java/racingcar/InputParser.java
+++ src/main/java/racingcar/InputParser.java
@@ -6,6 +6,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InputParser {
+
+    private final Integer ROUND_LIMIT = Integer.MAX_VALUE;
+    private final String NAME_EXCEPTION_MESSAGE = "자동차 이름의 길이는 1 이상, 5 이하여야 합니다.";
+    private final String ROUND_EXCEPTION_MESSAGE = "라운드의 횟수는 0 ~ " + ROUND_LIMIT + " 사이의 정수이어야 합니다.";
+
+
     public ArrayList<String> getInputNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputNames = readLine();
@@ -21,15 +27,24 @@ public class InputParser {
     public Integer getRound() {
         System.out.println("시도할 횟수는 몇 회인가요?");
         try {
-            return Integer.parseInt(readLine());
+            Integer round = Integer.parseInt(readLine());
+            checkRoundIsValid(round);
+
+            return round;
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("횟수 입력은 숫자여야 합니다.");
+            throw new IllegalArgumentException(ROUND_EXCEPTION_MESSAGE);
         }
     }
 
     public void checkNameIsValid(String name) {
         if (name.length() > 5) {
-            throw new IllegalArgumentException("이름은 5자 이하여아 합니다.");
+            throw new IllegalArgumentException(NAME_EXCEPTION_MESSAGE);
+        }
+    }
+
+    public void checkRoundIsValid(Integer round) {
+        if (round > ROUND_LIMIT || round < 0) {
+            throw new IllegalArgumentException(ROUND_EXCEPTION_MESSAGE);
         }
     }
 }
