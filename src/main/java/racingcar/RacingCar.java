811af4407fc4ea99715512a32096afc54d6d2188
KiSeungMin <ksm011212@gmail.com>
2025-10-21 17:36:51 +0900

    feat(RacingCar): 랜덤값 필드 및 업데이트 메서드 추가

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,13 +1,17 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RacingCar {
 
     private Integer position;
     private final String name;
+    private Integer randomValue;
 
     public RacingCar(String name) {
         this.name = name;
         this.position = 0;
+        this.randomValue = 0;
     }
 
     public RacingCar(String name, Integer position) {
@@ -29,5 +33,15 @@ public class RacingCar {
 
     public void printCurrentPosition() {
         System.out.println(name + " : " + "-".repeat(position));
+    }
+
+    public void updateRandomValue() {
+        this.randomValue = Randoms.pickNumberInRange(0, 9);
+    }
+
+    public void moveIfRandomValueMoreThan(Integer lowerBound) {
+        if (randomValue > lowerBound) {
+            this.addPosition(1);
+        }
     }
 }
